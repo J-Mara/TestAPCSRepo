2c00b7cd97755a0a373afbefebae6daf6a90a4ce
J-Mara <jmaragakis20@stuy.edu>
2020-10-20 21:17:35 -0400

    Assignment 9, first commit

---
+++ Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+  public static void printLoop(int n){
+    for(int i = 1; i <= n; i++){
+      for(int j = n; i <= j; j--){
+         System.out.print(i);
+      }
+      System.out.println();
+    }
+  }
+public static void main(String[] args) {
+   int a = Integer.parseInt(args[0]);
+   printLoop(a);
+  }
+}
